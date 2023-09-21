b86f53e49eb17f243490532c1d2999b07db67f92
linder <achielinder@gmail.com>
2023-09-21 05:28:40 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 2;
+let num2 =31;
+let multiply = num1 *num2;
+
+let random = 10
+Math.random(10);
+
+let num3 =4;
+let  num4 = 10;
+let mod = num3 % num4
+//=>4
+let max = 20;
+Math.max(1,5,10,20);
+
